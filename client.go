0a7af3b23d47b92ebe6d0a713d6f12f597c007de
Tim <info@airskip.app>
2026-05-31 13:36:45 -0500

    release: v1.1.3

--- client.go
+++ client.go
@@ -17,7 +17,7 @@ import (
 const DefaultBaseURL = "https://api.peyeeye.ai"
 
 // Version of this SDK. Reflected in the User-Agent header.
-const Version = "1.1.2"
+const Version = "1.1.3"
 
 const userAgent = "peyeeye-go/" + Version
 
